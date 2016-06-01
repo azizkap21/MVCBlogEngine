e028dadc8126446155c05062f7488a33d4ff8fda
verveinfotech <info@verveinfotech.com>
2016-06-01 15:08:44 +0100

    Some more projects have been added

---
+++ MVCBlogEngine.Core/TransactionalModels/EntityResultBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBlogEngine.Core.TransactionalModels
+{
+    public class EntityResultBase
+    {
+
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+        
+        public string SuccessMessage { get; set; }
+                
+    }
+}
